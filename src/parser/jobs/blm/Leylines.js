46f8405b8fb284a938e826cd43996cfdf9b659eb
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2018-09-30 11:16:02 -0500

    Accordion button zoom to timeline segment

--- src/parser/jobs/blm/Leylines.js
+++ src/parser/jobs/blm/Leylines.js
@@ -5,7 +5,7 @@ import {Rule, Requirement} from 'parser/core/modules/Checklist'
 import Module from 'parser/core/Module'
 import {ActionLink} from 'components/ui/DbLink'
 import {i18nMark, Trans} from '@lingui/react'
-import {Accordion} from 'semantic-ui-react'
+import {Accordion, Button} from 'semantic-ui-react'
 import {Group, Item} from 'parser/core/modules/Timeline'
 import DISPLAY_ORDER from './DISPLAY_ORDER'
 
@@ -27,19 +27,12 @@ export default class Leylines extends Module {
 		current: null,
 		history: [],
 	}
-	_castingEvent = null
 	_buffs = {}
 	_group = null
 
 	constructor(...args) {
 		super(...args)
 
-		this.addHook('begincast', {
-			by: 'player',
-		}, this._onBeginCast)
-		this.addHook('cast', {
-			by: 'player',
-		}, this._onCast)
 		this.addHook('applybuff', {
 			by: 'player',
 			abilityId: STATUSES.CIRCLE_OF_POWER.id,
@@ -93,30 +86,6 @@ export default class Leylines extends Module {
 		})
 	}
 
-	_onBeginCast(event) {
-		if (this._leyLines.current) {
-			if (!this._leyLines.current.stop) {
-				this._castingEvent = event
-			}
-		}
-	}
-
-	_onCast(event) {
-		if (this._castingEvent) {
-			this._castingEvent = null
-		}
-		if (this._leyLines.current) {
-			if (!this._leyLines.current.stop) {
-				this._leyLines.current.casts.push(event)
-			}
-		}
-		if (this._circleOfPowers.current) {
-			if (!this._circleOfPowers.current.stop) {
-				this._circleOfPowers.current.casts.push(event)
-			}
-		}
-	}
-
 	_onLoseBuff(event) {
 		const item = this.getTargetBuffs(event)[event.ability.guid]
 		// This shouldn't happen, but it do.
@@ -132,7 +101,6 @@ export default class Leylines extends Module {
 		this._leyLines.current = {
 			start: event.timestamp,
 			stop: null,
-			casts: [],
 		}
 
 		this._onApplyBuff(event)
@@ -141,9 +109,6 @@ export default class Leylines extends Module {
 	_onRemoveLeyLines(event) {
 		if (this._leyLines.current) {
 			this._leyLines.current.stop = event.timestamp
-			if (this._castingEvent) {
-				this._leyLines.current.casts.push(this._castingEvent)
-			}
 		}
 		this._onLoseBuff(event)
 	}
@@ -154,7 +119,6 @@ export default class Leylines extends Module {
 		this._circleOfPowers.current = {
 			start: event.timestamp,
 			stop: null,
-			casts: [],
 		}
 		this._onApplyBuff(event) // Also track application time for UI
 	}
@@ -162,9 +126,6 @@ export default class Leylines extends Module {
 	_onRemoveCircleOfPower(event) {
 		if (this._circleOfPowers.current) {
 			this._circleOfPowers.current.stop = event.timestamp
-			if (this._castingEvent) {
-				this._circleOfPowers.current.casts.push(this._castingEvent)
-			}
 		}
 		this._onLoseBuff(event) // Also track application time for accordion UI
 	}
@@ -177,18 +138,12 @@ export default class Leylines extends Module {
 		if (this._circleOfPowers.current) {
 			if (!this._circleOfPowers.current.stop) {
 				this._circleOfPowers.current.stop = event.timestamp
-				if (this._castingEvent) {
-					this._circleOfPowers.current.casts.push(event)
-				}
 			}
 			this._circleOfPowers.history.push(this._circleOfPowers.current)
 		}
 		if (this._leyLines.current) {
 			if (!this._leyLines.current.stop) {
 				this._leyLines.current.stop = event.timestamps
-				if (this._castingEvent) {
-					this._leyLines.current.casts.push(this._castingEvent)
-				}
 			}
 			this._leyLines.history.push(this._leyLines.current)
 		}
@@ -196,7 +151,7 @@ export default class Leylines extends Module {
 		if (!this._circleOfPowers.history.length) { return }
 		if (!this._leyLines.history.length) { return }
 
-		//filter out the last possible LL usage because it would make things weird.  - STOPPING THIS NOW
+		// Get the total duration of CoP uptime and ley lines, so we can get the overall percentage uptime
 		const circleOfPowerDurations = this._circleOfPowers.history.map(cops => Math.max(cops.stop - cops.start, 0))
 		const sumOfCoPUpTime = circleOfPowerDurations.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
 		const leyLinesDurations = this._leyLines.history.map(lines => Math.max(lines.stop - lines.start, 0))
@@ -221,6 +176,8 @@ export default class Leylines extends Module {
 			// Get the uptime percentage of Circle of Power for this Ley Lines usage
 			const thisCoPHistory = this._circleOfPowers.history.filter(cops => ((cops.start >= leyLinesEvent.start) & (cops.stop <= leyLinesEvent.stop)))
 			const thisCoPUptime = thisCoPHistory.map(cops => Math.max(cops.stop - cops.start, 0)).reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+			// Note that since we're getting the actual duration, rather than the expected duration, technically we'll call it 100% uptime if you stay in the lines and die halfway through...
+			// However, since that'll get flagged as a morbid checklist item, that's probably ok.
 			const thisLeyLinesDuration = leyLinesEvent.stop - leyLinesEvent.start
 			const thisPercent = this._percentFunction(thisLeyLinesDuration, thisCoPUptime).toFixed(2)
 
@@ -232,7 +189,8 @@ export default class Leylines extends Module {
 						&nbsp;-&nbsp;{thisPercent}% uptime
 					</Fragment>,
 				},
-				content: { // TODO: Revising after pulling changes to show timeline
+				content: {
+					content: <Button onClick={() => this.timeline.show(leyLinesEvent.start - this.parser.fight.start_time, leyLinesEvent.stop - this.parser.fight.start_time)}><Trans id="blm.leylines.timelinebutton">View Timeline</Trans></Button>,
 				},
 			}
 		})
